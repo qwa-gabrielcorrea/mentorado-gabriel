09c5a52e55e749c30953cbd5a49c20ceb891088a
qwa-gabrielcorrea <gabriel.correa@qwasolucoes.com.br>
2025-10-24 11:19:14 -0300

    feat: Implementacao Pilha

--- src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/PilhaProvider.java
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/PilhaProvider.java
@@ -1,27 +1,40 @@
 package br.com.qwasolucoes.mentoria.implementacoes.algoritmos;
 
+import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 
 import br.com.qwasolucoes.mentoria.interfaces.algoritmos.Pilha;
 
 public class PilhaProvider implements Pilha {
 
+	List<Object> lista = new ArrayList<>();
+
 	@Override
 	public void empilhar(Object elemento) {
 		 
+        this.lista.add(elemento);
 		
 	}
 
 	@Override
 	public Object desempilhar() {
 		 
-		return null;
+		if (this.lista.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return this.lista.remove(this.lista.size() - 1);
 	}
 
 	@Override
 	public List obterTodos() {
 		 
-		return null;
+		if (this.lista.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return lista;
 	}
 
 	
