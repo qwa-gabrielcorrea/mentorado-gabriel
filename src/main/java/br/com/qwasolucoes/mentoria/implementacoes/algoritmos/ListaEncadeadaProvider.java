e6a5e080cc331d129e032e26e268741867595a5a
qwa-gabrielcorrea <gabriel.correa@qwasolucoes.com.br>
2025-10-28 15:09:58 -0300

    feat: implementacao lista ligada

--- src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/ListaEncadeadaProvider.java
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/ListaEncadeadaProvider.java
@@ -1,10 +1,22 @@
 package br.com.qwasolucoes.mentoria.implementacoes.algoritmos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.qwasolucoes.mentoria.interfaces.algoritmos.ListaEncadeada;
 
 public class ListaEncadeadaProvider implements ListaEncadeada {
+
+	private int tamanho; 
+	private Node head;
+	private Node tail;
+
+	public ListaEncadeadaProvider(){
+		this.tamanho = 0; 
+	}
+
+	List<Object> listaEncadeada = new ArrayList<>();
+
 
 	@Override
 	public void remover() {
@@ -14,18 +26,38 @@ public class ListaEncadeadaProvider implements ListaEncadeada {
 	@Override
 	public void adicionar(Object elemento) {
 
+		Node novoNo = new Node(elemento);
+
+		if(this.head == null && this.tail == null){
+			this.head = novoNo;
+			this.tail = novoNo;
+		} else {
+			this.tail.setNext(novoNo);
+			this.tail = novoNo;
+		}
+
+		this.tamanho ++;
+		listaEncadeada.add(novoNo);
+
 	}
 
 	@Override
 	public List todos() {
 
-		return null;
+		return listaEncadeada;
 	}
 
 	@Override
 	public Object obter(int indice) {
 
-		return null;
+		Node atual = this.head;
+		for (int i = 0; i < indice; i++){
+			if(atual.getNext() != null){
+				atual = ataul.getNext();
+			}
+		}
+
+		return atual;
 	}
 
 }
---
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/Node.java
@@ -0,0 +1,25 @@
+package br.com.qwasolucoes.mentoria.implementacoes.algoritmos;
+
+public class Node {
+
+    private Object value; 
+    private Object next;
+
+    public Node(Node novoNo){
+        this.value = value;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    public Object getNext() {
+        return next;
+    }
+    public void setNext(Object next) {
+        this.next = next;
+    }
+    
+}
