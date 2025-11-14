d4b370b1fe2a7a18ad447f577a8bb67091c3f946
qwa-gabrielcorrea <gabriel.correa@qwasolucoes.com.br>
2025-11-14 10:32:55 -0300

    fix: recursao

--- src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/RecursaoProvider.java
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/algoritmos/RecursaoProvider.java
@@ -7,22 +7,21 @@ public class RecursaoProvider implements Recursao {
     @Override
     public int recursao(int valor, int limite) {
 
-        if (valor > limite) {
+        if (valor >= limite) {
             return 0;
         }
-        
-        if (valor <= 1){
-            return 1;
-        }
 
-        return recursao(valor + 1, limite);
+        return 1 + recursao(valor + 1, limite);
     }
 
     @Override
     public int recursaoSoma(int valor, int limite) {
 
+        if (valor > limite) {
+            return 0;
+        }
         if (valor == limite) {
-            return limite;
+            return limite; 
         }
 
         return valor + recursaoSoma(valor + 1, limite);
@@ -31,12 +30,16 @@ public class RecursaoProvider implements Recursao {
     @Override
     public int recursaoFibonacci(int valor, int limite) {
 
+        if (valor > limite) {
+            return -1;
+        }
+
         if (valor <= 0) {
             return 0;
         }
 
         if (valor == 1) {
-            return 1;
+            return 1; 
         }
 
         return recursaoFibonacci(valor - 1, limite) + recursaoFibonacci(valor - 2, limite);
