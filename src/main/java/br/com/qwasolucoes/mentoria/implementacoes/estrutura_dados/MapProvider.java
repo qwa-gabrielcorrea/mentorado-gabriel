f5c567a721a35cbaec0da2098038e5c11ec3ffba
qwa-gabrielcorrea <gabriel.correa@ext.evertecinc.com.br>
2025-12-04 14:40:11 -0300

    feat: implementação método nomeTelefones

--- src/main/java/br/com/qwasolucoes/mentoria/implementacoes/estrutura_dados/MapProvider.java
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/estrutura_dados/MapProvider.java
@@ -127,14 +127,48 @@ public class MapProvider implements EstruturaDadosMap{
 
 	@Override
 	public List<String> valoresMapa(Map<String, String> mapa) {
+		
+		List<String> valores = new ArrayList<>();
+		
+		for(String valor : mapa.keySet()) {
+			valores.add(valor);
+		}
 		 
-		return null;
+		return valores;
 	}
 
 	@Override
 	public Map<String, List<Integer>> nomeTelefones(int limite) {
-		 
-		return null;
+	
+		Map<String, List<Integer>> mapa = new TreeMap<>();
+		
+		List<Integer> pares = new ArrayList<>();
+		List<Integer> impares = new ArrayList<>();
+		List<Integer> primos = new ArrayList<>();
+		List<Integer> multip = new ArrayList<>();
+		
+		for (int i = 0; i <= limite; i++) {
+			
+			if(i % 2 == 0) {
+				pares.add(i);
+			} else {
+				impares.add(i);
+			}
+			if(validaPrimo(i)) {
+				primos.add(i);
+			}
+			if (i %10 == 0) {
+				multip.add(i);
+			}
+
+		}
+		
+		mapa.put("NUMEROS_IMPARES", pares);
+		mapa.put("NUMEROS_PARES", impares);
+		mapa.put("NUMEROS_PRIMOS", primos);
+		mapa.put("NUMEROS_MULTIPLOS_10", multip);
+		
+		return mapa;
 	}
 
 	@Override
@@ -148,4 +182,22 @@ public class MapProvider implements EstruturaDadosMap{
 
 		return null;
 	}
+	
+	
+	public boolean validaPrimo (Integer valor){
+
+		if(valor < 2){
+			return false;
+		}
+
+		for (int i = 2; i * i <= valor; i++){
+			if (valor % i == 0){
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
+
+
