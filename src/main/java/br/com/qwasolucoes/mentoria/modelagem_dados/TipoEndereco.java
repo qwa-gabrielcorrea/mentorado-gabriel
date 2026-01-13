1b69dfe4775222746e5df88fee2bc11d0a780053
qwa-gabrielcorrea <gabriel.correa@ext.evertecinc.com.br>
2026-01-13 08:12:40 -0300

    correção da versão do java e configuração do UTF-8

--- src/main/java/br/com/qwasolucoes/mentoria/modelagem_dados/TipoEndereco.java
+++ src/main/java/br/com/qwasolucoes/mentoria/modelagem_dados/TipoEndereco.java
@@ -2,23 +2,5 @@ package br.com.qwasolucoes.mentoria.modelagem_dados;
 
 public class TipoEndereco {
 	
-	String tipo;
-
-	public TipoEndereco(String tipo) {
-		this.tipo = tipo;
-	}
-	
-	public TipoEndereco() {
-		
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	} 
-	
 
 }
