212fa771f2584ae4361940f728c03d53054d23ee
qwa-gabrielcorrea <gabriel.correa@ext.evertecinc.com.br>
2026-01-19 13:06:30 -0300

    Chamada do Método Iniciar - Relacionamento

--- src/main/java/br/com/qwasolucoes/mentoria/implementacoes/relacionamento/RelacionamentoProvider.java
+++ src/main/java/br/com/qwasolucoes/mentoria/implementacoes/relacionamento/RelacionamentoProvider.java
@@ -22,6 +22,10 @@ import br.com.qwasolucoes.mentoria.modelagem_dados.Pessoa;
 import br.com.qwasolucoes.mentoria.modelagem_dados.Profissao;
 
 public class RelacionamentoProvider implements Relacionamentos {
+		
+	public RelacionamentoProvider() {
+		iniciar();
+	}
 
 	List<Pessoa> listaPessoas = new ArrayList<>();
 	List<Endereco> listaEnderecos = new ArrayList<>();
@@ -30,16 +34,6 @@ public class RelacionamentoProvider implements Relacionamentos {
 	List<Empresa> listaEmpresas = new ArrayList<>();
 	List<Escolaridade> listaEscolaridade = new ArrayList<>();
 	List<Instituicao> listaInstituicoes = new ArrayList<>();
-
-	public static void main(String[] args) {
-
-		RelacionamentoProvider iniciaClasse = new RelacionamentoProvider();
-		iniciaClasse.iniciar();
-
-		String classpath = System.getProperty("java.class.path");
-		System.out.println(classpath);
-
-	}
 
 	@Override
 	public void iniciar() {
